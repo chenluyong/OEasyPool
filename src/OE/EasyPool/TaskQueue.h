67d762011b822ea3a6accad5d1a14b247c590820
LuYong Chen <727057301@qq.com>
2017-06-13 15:28:56 +0800

    [MOD] 重新给关键的成员变量配备了线程锁

--- src/OE/EasyPool/TaskQueue.h
+++ src/OE/EasyPool/TaskQueue.h
@@ -48,77 +48,83 @@ class OETaskQueue {
 
 private:
 
-	/// 就绪任务队列
+    /// 就绪任务队列
     std::deque<std::shared_ptr<T> > queue_;
+    std::mutex mutexQueue_;
     /// 运行任务map
     std::unordered_map<size_t, std::shared_ptr<T> > mapDoingTask_;
-	/// 互斥量
-	std::mutex mutex_;
-	/// 条件变量
-	std::condition_variable conditPut_;
+    std::mutex mutexDoingTask_;
+    /// 条件变量
+    std::condition_variable conditPut_;
+    std::mutex mutexConditPut_;
 
 public:
 
-	/**
-	* @brief ：向队列的末尾插入任务
-	* @param ：task 任务类
-	*/
+    /**
+    * @brief ：向队列的末尾插入任务
+    * @param ：task 任务类
+    */
     void put_back(std::shared_ptr<T> &task) {
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::unique_lock<std::mutex> lock(mutexQueue_);
 
         queue_.push_back(task);
 
         conditPut_.notify_one();
     }
-	/**
-	* @brief ：向队列的头部插入任务
-	* @param ：task 任务类
-	*/
+    /**
+    * @brief ：向队列的头部插入任务
+    * @param ：task 任务类
+    */
     void put_front(std::shared_ptr<T> &task) {
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::unique_lock<std::mutex> lock(mutexQueue_);
 
         queue_.push_front(task);
 
         conditPut_.notify_one();
     }
-	/**
-	* @brief ：获取队首（并将任务加到运行任务列表中）
-	* @return：任务类
-	*/
+    /**
+    * @brief ：获取队首（并将任务加到运行任务列表中）
+    * @return：任务类
+    */
     std::shared_ptr<T> get(void) {
 
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::unique_lock<std::mutex> lock(mutexQueue_);
 
         if (queue_.empty())
             return nullptr;
 
 
+        std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
         std::shared_ptr<T> &task = queue_.front();
-        mapDoingTask_.insert(make_pair(task->getID(), task));
+        mapDoingTask_.insert(std::make_pair(task->getID(), task));
         queue_.pop_front();
 
         return task;
     }
-	/**
-	* @brief ：获取整个双向链表的大小
-	* @return：大小
-	*/
+    /**
+    * @brief ：获取整个双向链表的大小
+    * @return：大小
+    */
     size_t size(void) {
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::unique_lock<std::mutex> lock(mutexQueue_);
         return queue_.size();
     }
 
-	/**
-	* @brief ：释放队列
-	*/
+    /**
+    * @brief ：释放队列
+    */
     void release(void) {
 
         //将就绪队列中的任务删除
-        std::unique_lock<std::mutex> lock(mutex_);
+        {
+            std::unique_lock<std::mutex> lock(mutexQueue_);
 
-        if (!queue_.empty())
-            queue_.clear();//清空
+            if (!queue_.empty())
+                queue_.clear();//清空
+        }
 
+        {
+        std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
 
         if (!mapDoingTask_.empty()) {
 
@@ -129,17 +135,19 @@ public:
                 it_map->second->setCancelRequired();
 
         }
+    }
+
 
         conditPut_.notify_all();
     }
-	/**
-	* @brief ：删除任务（从就绪队列删除，如果就绪队列没有，则看执行队列有没有，有的话置下取消状态位）
-	* @param ：nID 任务的编号
-	* @return：成功返回0 失败返回非0
-	*/
+    /**
+    * @brief ：删除任务（从就绪队列删除，如果就绪队列没有，则看执行队列有没有，有的话置下取消状态位）
+    * @param ：nID 任务的编号
+    * @return：成功返回0 失败返回非0
+    */
     int deleteTask(size_t nID) {
 
-        std::unique_lock<std::mutex> lock(mutex_, std::defer_lock);
+        std::unique_lock<std::mutex> lock(mutexQueue_, std::defer_lock);
         lock.lock();
 
 
@@ -152,15 +160,19 @@ public:
                 return 0;
             }
         }
-
-        // std::unordered_map<int64_t, std::shared_ptr<OETask>>::iterator
-        auto it_map = mapDoingTask_.find(nID);
-        if (it_map != mapDoingTask_.end())
-            it_map->second->setCancelRequired();
-
-
         //下面的逻辑可能会造成死锁，这里要及时释放
         lock.unlock();
+
+        // 试图取消正在执行的任务
+        {
+            std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
+            // std::unordered_map<int64_t, std::shared_ptr<OETask>>::iterator
+            auto it_map = mapDoingTask_.find(nID);
+            if (it_map != mapDoingTask_.end())
+                it_map->second->setCancelRequired();
+        }
+
+
 
 
         //任务执行完后再返回
@@ -169,29 +181,33 @@ public:
 
         return 0;
     }
-	/**
-	* @brief ：删除所有任务
-	* @return：成功返回0 失败返回非0
-	*/
+    /**
+    * @brief ：删除所有任务
+    * @return：成功返回0 失败返回非0
+    */
     int deleteAllTasks(void) {
 
-        std::unique_lock<std::mutex> lock(mutex_, std::defer_lock);
+
+        std::unique_lock<std::mutex> lock(mutexQueue_, std::defer_lock);
         lock.lock();
 
         if (!queue_.empty())
             queue_.clear();//清空
 
 
-        if (!mapDoingTask_.empty()) {
+        {
+            std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
+            if (!mapDoingTask_.empty()) {
 
-            // std::unordered_map<int64_t, std::shared_ptr<OETask>>::iterator
-            auto it_map = mapDoingTask_.begin();
+                // std::unordered_map<int64_t, std::shared_ptr<OETask>>::iterator
+                auto it_map = mapDoingTask_.begin();
 
-            for (; it_map != mapDoingTask_.end(); ++it_map)
-                it_map->second->setCancelRequired();
+                for (; it_map != mapDoingTask_.end(); ++it_map)
+                    it_map->second->setCancelRequired();
 
+            }
         }
-        //下面的逻辑可能会造成死锁，这里要及时释放
+
         lock.unlock();
 
 
@@ -202,14 +218,14 @@ public:
 
         return 0;
     }
-	/**
-	* @brief ：任务完成回调（从运行列表中删除指定任务）
-	* @param ：nID 任务的编号
-	* @return：成功返回0 失败返回非0
-	*/
+    /**
+    * @brief ：任务完成回调（从运行列表中删除指定任务）
+    * @param ：nID 任务的编号
+    * @return：成功返回0 失败返回非0
+    */
     int onTaskFinished(size_t nID) {
 
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
 
         // std::unordered_map<size_t, std::shared_ptr<OETask>>::iterator
         auto it_map = mapDoingTask_.find(nID);
@@ -219,13 +235,13 @@ public:
 
         return 0;
     }
-	/**
-	* @brief ：判断任务是否执行完毕
-	* @param ：nID 任务的编号
-	* @return：任务类
-	*/
+    /**
+    * @brief ：判断任务是否执行完毕
+    * @param ：nID 任务的编号
+    * @return：任务类
+    */
     std::shared_ptr<T> isTaskProcessed(size_t nId) {
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::lock_guard<std::mutex> lock_queue(mutexQueue_);
         // std::deque<std::shared_ptr<OETask>>::iterator
         auto it = queue_.begin();
         for (; it != queue_.end(); ++it) {
@@ -235,6 +251,7 @@ public:
 
         }
 
+        std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
         // std::unordered_map<int64_t, std::shared_ptr<OETask>>::iterator
         auto it_map = mapDoingTask_.find(nId);
         if (it_map != mapDoingTask_.end())
@@ -244,13 +261,13 @@ public:
         return nullptr;
     }
 
-	/**
-	* @brief ：等待有任务到达（带超时：超时自动唤醒）
-	* @param ：millsec 超时时间（毫秒）
-	* @return：成功返回true 失败返回false
-	*/
+    /**
+    * @brief ：等待有任务到达（带超时：超时自动唤醒）
+    * @param ：millsec 超时时间（毫秒）
+    * @return：成功返回true 失败返回false
+    */
     bool wait(std::chrono::milliseconds millsec) {
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::unique_lock<std::mutex> lock(mutexConditPut_);
         conditPut_.wait_for(lock, millsec);
 
         return true;
