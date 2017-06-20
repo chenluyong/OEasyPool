6b89c4899d0711a4942b7086eba43e7052358e45
LuYong Chen <727057301@qq.com>
2017-06-20 12:12:07 +0800

    [DEL] 删除重复的代码

--- src/OE/EasyPool/TaskQueue.h
+++ src/OE/EasyPool/TaskQueue.h
@@ -42,20 +42,25 @@
 #include <thread>
 
 
-/// 任务队列类
+// 1. 添加任务  (put)
+// 2. 领取任务  (get)
+// 3. 完成任务  (onTaskFinished)
+// 4. 进入等待  (wait)
+// end. 释放任务列表 (release)
+/// @class : 任务队列类
 template<class T>
 class OETaskQueue {
 
 private:
 
     /// 就绪任务队列                               
-    std::mutex										mutexQueue_;
-    std::deque<std::shared_ptr<T> >					queue_;
+    std::mutex                          mutexQueue_;
+    std::deque<std::shared_ptr<T> >     queue_;
     /// 条件变量                                      
-    std::mutex										mutexConditPut_;
-    std::condition_variable							conditPut_;
+    std::mutex                          mutexConditPut_;
+    std::condition_variable             conditPut_;
     /// 运行任务map                                   
-    std::mutex										mutexDoingTask_;
+    std::mutex                          mutexDoingTask_;
     std::unordered_map<size_t, std::shared_ptr<T> > mapDoingTask_;
 
 public:
@@ -64,8 +69,7 @@ public:
     * @brief ：向队列的末尾插入任务
     * @param ：task 任务类
     */
-    void 
-	put_back(std::shared_ptr<T> &task) {
+    void put_back(std::shared_ptr<T> &task) {
         std::unique_lock<std::mutex> lock(mutexQueue_);
 
         queue_.push_back(task);
@@ -76,8 +80,7 @@ public:
     * @brief ：向队列的头部插入任务
     * @param ：task 任务类
     */
-    void 
-	put_front(std::shared_ptr<T> &task) {
+    void put_front(std::shared_ptr<T> &task) {
         std::unique_lock<std::mutex> lock(mutexQueue_);
 
         queue_.push_front(task);
@@ -88,8 +91,7 @@ public:
     * @brief ：获取队首（并将任务加到运行任务列表中）
     * @return：任务类
     */
-    std::shared_ptr<T> 
-	get(void) {
+    std::shared_ptr<T> get(void) {
 
         std::unique_lock<std::mutex> lock(mutexQueue_);
 
@@ -108,8 +110,7 @@ public:
     * @brief ：获取整个双向链表的大小
     * @return：大小
     */
-    size_t 
-	size(void) {
+    size_t size(void) {
         std::unique_lock<std::mutex> lock(mutexQueue_);
         return queue_.size();
     }
@@ -117,30 +118,9 @@ public:
     /**
     * @brief ：释放队列
     */
-    void 
-	release(void) {
+    void release(void) {
 
-        //将就绪队列中的任务删除
-        {
-            std::unique_lock<std::mutex> lock(mutexQueue_);
-
-            if (!queue_.empty())
-                queue_.clear();//清空
-        }
-
-        {
-        std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
-
-        if (!mapDoingTask_.empty()) {
-
-            // std::unordered_map<int64_t, std::shared_ptr<OETask>>::iterator
-            auto it_map = mapDoingTask_.begin();
-
-            for (; it_map != mapDoingTask_.end(); ++it_map)
-                it_map->second->setCancelRequired();
-
-        }
-    }
+        deleteAllTasks();
 
 
         conditPut_.notify_all();
@@ -150,8 +130,7 @@ public:
     * @param ：nID 任务的编号
     * @return：成功返回0 失败返回非0
     */
-    int 
-	deleteTask(size_t nID) {
+    int deleteTask(size_t nID) {
 
         std::unique_lock<std::mutex> lock(mutexQueue_, std::defer_lock);
         lock.lock();
@@ -191,8 +170,7 @@ public:
     * @brief ：删除所有任务
     * @return：成功返回0 失败返回非0
     */
-    int 
-	deleteAllTasks(void) {
+    int deleteAllTasks(void) {
 
 
         std::unique_lock<std::mutex> lock(mutexQueue_, std::defer_lock);
@@ -230,8 +208,7 @@ public:
     * @param ：nID 任务的编号
     * @return：成功返回0 失败返回非0
     */
-    int 
-	onTaskFinished(size_t nID) {
+    int onTaskFinished(size_t nID) {
 
         std::lock_guard<std::mutex> lock_doing_task(mutexDoingTask_);
 
@@ -248,8 +225,7 @@ public:
     * @param ：nID 任务的编号
     * @return：任务类
     */
-    std::shared_ptr<T> 
-	isTaskProcessed(size_t nId) {
+    std::shared_ptr<T> isTaskProcessed(size_t nId) {
         std::lock_guard<std::mutex> lock_queue(mutexQueue_);
         // std::deque<std::shared_ptr<OETask>>::iterator
         auto it = queue_.begin();
@@ -275,8 +251,7 @@ public:
     * @param ：millsec 超时时间（毫秒）
     * @return：成功返回true 失败返回false
     */
-    bool 
-	wait(std::chrono::milliseconds millsec) {
+    bool wait(std::chrono::milliseconds millsec) {
         std::unique_lock<std::mutex> lock(mutexConditPut_);
         conditPut_.wait_for(lock, millsec);
 
